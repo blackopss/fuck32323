c303c2382fc14b3c2b49276a7de23bf83f8761ca
blackopss <36933564+blackopss@users.noreply.github.com>
2018-08-08 03:38:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -276,9 +276,10 @@ if (args.length == 0 && dispatcher) {
 break;
 
 		case "help":
+
+message.channel.send(
 							const embed1 = new Discord.RichEmbed()
 
-message.channel.send(`
 #User
 **${prefix}userinfo** [Username or blank] : displays info for the selected user or for message sender if arguments are not provided
 #Music
@@ -293,8 +294,9 @@ message.channel.send(`
 **${prefix}yt** [Search Term] : Searches The YouTube and Returns First 5 Results
 **${prefix}add** [Search Result Number] : adds a Song From YouTube Search To The Queue
 **${prefix}vol** [percentage] : Sets The Volume of The Music To Given Percentage
-`);
 		message.channel.send(embed1);
+
+);
 
 break;
 
